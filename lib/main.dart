9e15efaaf9c842c86ca13ea8b84441e192584ec3
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-05-29 15:42:53 +0300

    added screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/choose_location.dart';
+import 'pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:
+    home: Home()
   ));
 }
 
